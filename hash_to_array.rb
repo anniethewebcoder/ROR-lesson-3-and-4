2de7864bf6b85d2e867532c6bd53e0ac7afd1d3b
anniethewebcoder <anniethewebcoder@gmail.com>
2024-07-17 01:34:59 -0400

    hash to array

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -1,5 +1,21 @@
-hash_set = {}
+user_hash = {}
 
-def user(answer)
+for i in (1..5)
+    puts ""
 
+    print "Enter Key #{i}: "
+    key = gets.chomp.capitalize
+
+    print "Enter Value #{i}: "
+    val = gets.chomp.capitalize
+
+    user_hash[i.to_s + "_" + key] = val
 end
+
+def user(hash)
+    hash.to_a.inspect
+end
+
+puts ""
+puts "Your array of inputs:"
+puts user(user_hash)
